621849bf9c27fd015b0066784d0b9c1ddc5e8e8f
kilhyeon <net94.tizona@naver.com>
2021-05-28 09:39:51 +0900

    초기화 및 database connection complete

---
+++ src/main/java/spring5_rest_study/config/ContextRoot.java
@@ -0,0 +1,16 @@
+package spring5_rest_study.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@Import({ContextDataSource.class})
+@ComponentScan(basePackages = {
+		  "spring5_rest_study.mappers",
+		  "spring5_rest_study.controller",
+		  "spring5_rest_study.service"}
+)
+public class ContextRoot {
+
+}
